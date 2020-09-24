d8149e9af03e59ca9f96e60a3df1b3f02f0a39e0
Kvík <martin.balaz@trojsten.sk>
2020-09-24 16:16:39 +0200

    Enabled non-ASCII usernames

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as authViews
 from . import views
 
 urlpatterns = [
-    path('user/<slug:username>',        views.account,                                                              name = 'account'),
-    path('login',                       authViews.LoginView.as_view(template_name = 'accounts/login.html'),         name = 'login'),
-    path('logout',                      authViews.LogoutView.as_view(template_name = 'accounts/logout.html'),       name = 'logout'),
+    re_path('user/(?P<username>\w+)',   views.account,                                                          name='account'),
+    path('login',                       authViews.LoginView.as_view(template_name='accounts/login.html'),       name='login'),
+    path('logout',                      authViews.LogoutView.as_view(template_name='accounts/logout.html'),     name='logout'),
 ]
